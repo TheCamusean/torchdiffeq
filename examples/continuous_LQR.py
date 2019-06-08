944f1b4ec397d318199c1c8c5bcd02f992affe75
Julen Urain De Jesus <urain@ias.informatik.tu-darmstadt.de>
2019-06-08 20:40:02 +0200

    PMP starting

---
+++ examples/continuous_LQR.py
@@ -0,0 +1,328 @@
+import os
+import argparse
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchdiffeq._impl.adjoint_julen import odeint_adjoint as odeint
+
+parser = argparse.ArgumentParser('ODE demo')
+parser.add_argument('--method', type=str, choices=['dopri5', 'adams'], default='dopri5')
+parser.add_argument('--data_size', type=int, default=1000)
+parser.add_argument('--batch_time', type=int, default=10)
+parser.add_argument('--batch_size', type=int, default=20)
+parser.add_argument('--niters', type=int, default=16000)
+parser.add_argument('--test_freq', type=int, default=20)
+parser.add_argument('--viz', action='store_true')
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--adjoint', action='store_true')
+
+parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--test_batch_size', type=int, default=1000)
+parser.add_argument('--tol', type=float, default=1e-3)
+args = parser.parse_args()
+
+
+device = torch.device('cuda:' + str(1) if torch.cuda.is_available() else 'cpu')
+
+
+# from optimal_control.LQR import LQR
+# ###################### System Dynamics and cost ######################
+#
+A = np.array([[2.,-0.3],[0.04,-0.01]])
+
+A = np.array([[2.,-3],[0.04,-0.01]])
+B = np.array([[10.0,-3.0],[0.0,3.0]])
+Qx = np.array([[20.,0.],[0.0,10.0]])
+Qu = np.array([[1,0],[0,1]])
+
+# lqr = LQR(A,B,Qx,Qu)
+#
+# t = np.linspace(0,25,100)
+# x0 = np.array([[-3., 10.]])
+#
+# y = lqr.optimal_traj(t,x0)
+#
+# y2 = lqr.passive_traj(t,x0)
+#
+# y = y.numpy()
+# plt.plot(t,y[:,0,0])
+# plt.plot(t,y[:,0,1])
+#
+# plt.figure()
+# y2 = y2.numpy()
+# plt.plot(t,y2[:,0,0])
+# plt.plot(t,y2[:,0,1])
+#
+# plt.show()
+
+#######################################################################
+
+
+
+true_y0 = torch.tensor([[36., -100.]])
+t = torch.linspace(0.01, 10., args.data_size)
+true_A = torch.tensor([[-0.1, 2.0], [-2.0, -0.1]])
+
+## Non-Linear System
+true_A_lin = torch.tensor([[1.5, 0], [0, 2]])
+true_A_pow2 = torch.tensor([[-1, 0], [0, -0.5]])
+true_A_mix = torch.tensor([[-0.5, -1.5]])
+
+
+# class Lambda(nn.Module):
+#
+#     def forward(self, t, y):
+#         lin = torch.mm(y, true_A_lin)
+#         pow2 = torch.mm(y**2, true_A_pow2)
+#         mix_coef = torch.tensor([[y[0][0]*y[0][1]]])
+#         mixed = torch.mm(mix_coef, true_A_mix)
+#         return torch.add(torch.add(lin, pow2),mixed)
+#
+# with torch.no_grad():
+#     true_y = odeint(Lambda(), true_y0, t, method='dopri5')
+
+t = torch.linspace(0.01, 25., args.data_size)
+true_y = torch.empty(1000,1,2).uniform_(-10,10)
+
+def get_batch():
+    s = torch.from_numpy(
+        np.random.choice(np.arange(args.data_size - args.batch_time, dtype=np.int64), args.batch_size, replace=False))
+
+    batch_y0 = true_y[s]  # (M, D)
+    batch_t = t[:args.batch_time]  # (T)
+    batch_y = torch.stack([true_y[s + i] for i in range(args.batch_time)], dim=0)  # (T, M, D)
+    return batch_y0, batch_t, batch_y
+
+class IntegerLoss(nn.Module):
+    def __init__(self):
+        super(IntegerLoss, self).__init__()
+
+
+        self.Q = torch.tensor(Qx).type(torch.FloatTensor)
+        self.R =  torch.tensor(Qu).type(torch.FloatTensor)
+
+
+    def forward(self, u, x):
+        d=3
+        uR = torch.matmul(u, R)
+        ut = torch.transpose(u, d - 2, d - 1)
+        uRu = torch.matmul(uR, ut)
+
+        xQ = torch.matmul(x, Q)
+        xt = torch.transpose(x, d - 2, d - 1)
+        xQx = torch.matmul(xQ, xt)
+
+        loss = xQx + uRu
+        return loss
+
+
+class Controller(nn.Module):
+    def __init__(self, dim_x, dim_u):
+        super(Controller, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(dim_x, 10),
+            nn.Tanh(),
+            nn.Linear(10, dim_u),
+            #nn.Tanh()
+            # nn.Linear(5, dim_u),
+        )
+
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
+
+    def forward(self, t, x):
+        return self.net(x)
+
+
+class system_linear_dynamics(nn.Module):
+    def __init__(self, A, B ):
+        super(system_linear_dynamics, self).__init__()
+        self.A = torch.tensor(A).type(torch.FloatTensor)
+        self.B = torch.tensor(B).type(torch.FloatTensor)
+
+    def forward(self, t, x, u):
+        dynamics_x = torch.matmul(x,self.A)
+        dynamics_u = torch.matmul(u,self.B)
+        dynamics = torch.add(dynamics_x,dynamics_u)
+        return dynamics
+
+
+class closed_loop_dynamics(nn.Module):
+    def __init__(self, linear_dyn, controller):
+        super(closed_loop_dynamics, self).__init__()
+
+        self.linear_dyn = linear_dyn
+        self.controller = controller
+
+    def forward(self, t, x):
+        u = self.controller(t,x)
+        x_1 = self.linear_dyn(t,x,u)
+        return x_1
+
+
+class ODEBlock(nn.Module):
+
+    def __init__(self, odefunc, integer_loss=None):
+        super(ODEBlock, self).__init__()
+        self.odefunc = odefunc
+        self.integration_time = torch.tensor([0, 1]).float()
+
+        self.integer_loss = integer_loss
+
+    def forward(self, x,t):
+        out = odeint(self.odefunc, x, t, rtol=args.tol, atol=args.tol, integer_loss= self.integer_loss)
+        return out
+
+    # @property
+    # def nfe(self):
+    #     return self.odefunc.nfe
+    #
+    # @nfe.setter
+    # def nfe(self, value):
+    #     self.odefunc.nfe = value
+
+
+
+class RunningAverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, momentum=0.99):
+        self.momentum = momentum
+        self.reset()
+
+    def reset(self):
+        self.val = None
+        self.avg = 0
+
+    def update(self, val):
+        if self.val is None:
+            self.avg = val
+        else:
+            self.avg = self.avg * self.momentum + val * (1 - self.momentum)
+        self.val = val
+
+
+
+
+
+Q =  torch.tensor(Qx).type(torch.FloatTensor)
+R =  torch.tensor(Qu).type(torch.FloatTensor)
+
+def loss_LQR(x,u, batch_t):
+    d = u.ndimension()
+
+    zero = torch.tensor([0.])
+    delta_T = batch_t[1:] - batch_t[:-1]
+    delta = torch.cat((delta_T,zero),0)
+
+    #uR = torch.matmul(u,R)
+    # ut = torch.transpose(u,d-2,d-1)
+    # uRu = torch.matmul(uR,ut)
+    # uRu = 0.025*uRu
+    #uRu = torch.matmul(delta.view(10,1,1,1),uRu)
+
+    x = x - 10
+    xQ = torch.matmul(x, Q)
+    xt = torch.transpose(x, d-2, d-1)
+    xQx = torch.matmul(xQ, xt)
+    #xQx = torch.matmul(delta.view(10,1,1,1),xQx)
+
+
+    # uRu_int = uRu.sum(0)
+    # xQx_int = xQx.sum(0)
+    #
+    # loss = torch.add(uRu_int,xQx_int)
+    #
+    # loss_clamp = torch.clamp(loss, min = -1000, max = 1000)
+
+    return xQx
+
+
+
+if __name__ == '__main__':
+
+    #################### Setup Learning Model ##################
+    ii = 0
+    dim = 2
+
+    integer_loss = IntegerLoss()
+
+    controller = Controller(dim,dim)
+    lin_dyn = system_linear_dynamics(A,B)
+    close_loop = closed_loop_dynamics(lin_dyn,controller)
+    func = ODEBlock(close_loop,integer_loss)
+
+
+
+    optimizer = torch.optim.SGD(func.parameters(), lr=1e-3, momentum=0.9)
+    #optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
+
+    end = time.time()
+
+
+    # lr_fn = learning_rate_with_decay(
+    #     args.batch_size, batch_denom=128, batches_per_epoch=batches_per_epoch, boundary_epochs=[60, 100, 140],
+    #     decay_rates=[1, 0.1, 0.01, 0.001]
+    # )
+
+    time_meter = RunningAverageMeter(0.97)
+    loss_meter = RunningAverageMeter(0.97)
+
+    ##############################################################
+
+     ################## Train iteration #########################
+    for itr in range(1, args.niters + 1):
+        optimizer.zero_grad()
+        batch_y0, batch_t, batch_y = get_batch()
+
+        pred_y = func(batch_y0, batch_t)
+        #pred_y2 = odeint(func2,batch_y0,batch_t)
+
+        pred_control = controller(batch_t,pred_y)
+        #print(pred_control)
+
+
+        loss = torch.mean(loss_LQR(pred_y,pred_control,batch_t))
+        print(loss)
+        info = loss.backward()
+        optimizer.step()
+        #print(loss)
+
+        #time_meter.update(time.time() - end)
+        #loss_meter.update(loss.item())
+
+        if itr % args.test_freq == 0:
+            with torch.no_grad():
+                pred_y = func(true_y0, t)
+                pred_control = controller(t, pred_y)
+
+                loss = loss_LQR(pred_y,pred_control,t)
+                loss = torch.mean(loss)
+
+                y_plot = pred_y.numpy()
+                timer = t.numpy()
+                #plt.close()
+                plt.clf()
+                plt.plot(timer,y_plot[:,0,0])
+                plt.plot(timer,y_plot[:,0,1])
+                plt.draw()
+
+                plt.pause(0.001)
+                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+
+
+                # print("Optimal parameters of LQR")
+                # print(lqr.optimal_controller())
+                # print("Learned parameters")
+                # for parameter in controller.parameters():
+                #     print(parameter)
+                ii += 1
+
---
+++ torchdiffeq/_impl/adjoint_julen.py
@@ -0,0 +1,189 @@
+import torch
+import torch.nn as nn
+from . import odeint
+from .misc import _flatten, _flatten_convert_none_to_zeros
+
+
+class OdeintAdjointMethod(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, *args):
+        assert len(args) >= 9, 'Internal error: all arguments required.'
+        y0, func, t, flat_params, rtol, atol, method, options, integer_loss = \
+            args[:-8], args[-8], args[-7], args[-6], args[-5], args[-4], args[-3], args[-2], args[-1]
+
+        ctx.func, ctx.rtol, ctx.atol, ctx.method, ctx.options, ctx.integer_loss = func, rtol, atol, method, options, integer_loss
+
+        with torch.no_grad():
+            ans = odeint(func, y0, t, rtol=rtol, atol=atol, method=method, options=options)
+        ctx.save_for_backward(t, flat_params, *ans)
+        return ans
+
+    @staticmethod
+    def backward(ctx, *grad_output):
+
+        t, flat_params, *ans = ctx.saved_tensors
+        ans = tuple(ans)
+        func, rtol, atol, method, options, integer_loss = ctx.func, ctx.rtol, ctx.atol, ctx.method, ctx.options, ctx.integer_loss
+        n_tensors = len(ans)
+        f_params = tuple(func.parameters())
+
+        # TODO: use a nn.Module and call odeint_adjoint to implement higher order derivatives.
+        def augmented_dynamics(t, y_aug):
+            # Dynamics of the original system augmented with
+            # the adjoint wrt y, and an integrator wrt t and args.
+            y, adj_y = y_aug[:n_tensors], y_aug[n_tensors:2 * n_tensors]  # Ignore adj_time and adj_params.
+
+            with torch.set_grad_enabled(True):
+                t = t.to(y[0].device).detach().requires_grad_(True)
+                y = tuple(y_.detach().requires_grad_(True) for y_ in y)
+
+                u = (func.base_func.controller(t,y[0]),)
+
+                y_1 = (func.base_func.linear_dyn(t,y[0],u[0]),)
+
+                loss_integer = integer_loss(u, y)
+
+                #func_eval = func(t, y)
+
+
+                dudy, *dudtheta = torch.autograd.grad(
+                    u, y + f_params, [torch.ones([20, 1, 2]), ],
+                    allow_unused=True, retain_graph=True
+                )
+
+                ############ We have to compute the gradient of the integer_loss with respect to u and with respect to x
+                dLdy, dLdu, *dLdtheta = torch.autograd.grad(
+                    loss_integer, y + u + f_params, [torch.ones([20,1,1]),],
+                    allow_unused=True, retain_graph=True
+                )
+
+                ### This grad is actually pretty smart. Using the autograd, he knows he is going to have a chain rule, of dl/dout dout/dx, so he is smart and says
+                ### as d lambda/dt = -lambda df()/dx , and lambda=grad_output dl/dout = lambda
+                vjp_t, vjp_y, dfdu, *vjp_y_and_params = torch.autograd.grad(
+                    y_1, (t,) + y + u + f_params,
+                    tuple(-adj_y_ for adj_y_ in adj_y), allow_unused=True, retain_graph=True
+                )
+                ## From total derivative to partial derivative
+                vjp_y = vjp_y - dfdu*dudy
+                dLdy  =  dLdy - dLdu*dudy
+
+
+
+
+
+            #vjp_y = vjp_y_and_params[:n_tensors]
+
+            vjp_params = vjp_y_and_params
+            #### Extra term from cost
+            #vjp_params = [a - b for a, b in zip(vjp_y_and_params, dLdtheta)]
+            #vjp_y = [a - b for a, b in zip(vjp_y, dLdy)]
+
+            # autograd.grad returns None if no gradient, set to zero.
+            vjp_t = torch.zeros_like(t) if vjp_t is None else vjp_t
+            vjp_y = tuple(torch.zeros_like(y_) if vjp_y_ is None else vjp_y_ for vjp_y_, y_ in zip(vjp_y, y))
+            vjp_params = _flatten_convert_none_to_zeros(vjp_params, f_params)
+
+            if len(f_params) == 0:
+                vjp_params = torch.tensor(0.).to(vjp_y[0])
+            return (*y_1, *vjp_y, vjp_t, vjp_params)
+
+        T = ans[0].shape[0]
+        with torch.no_grad():
+            adj_y = tuple(grad_output_[-1] for grad_output_ in grad_output)
+            adj_params = torch.zeros_like(flat_params)
+            adj_time = torch.tensor(0.).to(t)
+            time_vjps = []
+            for i in range(T - 1, 0, -1):
+
+                ans_i = tuple(ans_[i] for ans_ in ans)
+                grad_output_i = tuple(grad_output_[i] for grad_output_ in grad_output)
+                func_i = func(t[i], ans_i)
+
+                # Compute the effect of moving the current time measurement point.
+                dLd_cur_t = sum(
+                    torch.dot(func_i_.reshape(-1), grad_output_i_.reshape(-1)).reshape(1)
+                    for func_i_, grad_output_i_ in zip(func_i, grad_output_i)
+                )
+                adj_time = adj_time - dLd_cur_t
+                time_vjps.append(dLd_cur_t)
+
+                # Run the augmented system backwards in time.
+                if adj_params.numel() == 0:
+                    adj_params = torch.tensor(0.).to(adj_y[0])
+                aug_y0 = (*ans_i, *adj_y, adj_time, adj_params)
+                aug_ans = odeint(
+                    augmented_dynamics, aug_y0,
+                    torch.tensor([t[i], t[i - 1]]), rtol=rtol, atol=atol, method=method, options=options
+                )
+
+                # Unpack aug_ans.
+                adj_y = aug_ans[n_tensors:2 * n_tensors]
+                adj_time = aug_ans[2 * n_tensors]
+                adj_params = aug_ans[2 * n_tensors + 1]
+
+                adj_y = tuple(adj_y_[1] if len(adj_y_) > 0 else adj_y_ for adj_y_ in adj_y)
+                if len(adj_time) > 0: adj_time = adj_time[1]
+                if len(adj_params) > 0: adj_params = adj_params[1]
+
+                adj_y = tuple(adj_y_ + grad_output_[i - 1] for adj_y_, grad_output_ in zip(adj_y, grad_output))
+
+                del aug_y0, aug_ans
+
+            time_vjps.append(adj_time)
+            time_vjps = torch.cat(time_vjps[::-1])
+
+            #result = (*adj_y, None, time_vjps, adj_params, None, None, None, None, None)
+            #print(result)
+
+            return (*adj_y, None, time_vjps, adj_params, None, None, None, None, None)
+
+
+def odeint_adjoint(func, y0, t, rtol=1e-6, atol=1e-12, method=None, options=None, integer_loss=None):
+
+    # We need this in order to access the variables inside this module,
+    # since we have no other way of getting variables along the execution path.
+    if not isinstance(func, nn.Module):
+        raise ValueError('func is required to be an instance of nn.Module.')
+
+    if (integer_loss != None):
+        if not isinstance(integer_loss, nn.Module):
+            raise ValueError('integer_loss is required to be an instance of nn.Module.')
+
+
+
+
+
+    tensor_input = False
+    if torch.is_tensor(y0):
+
+        class TupleFunc(nn.Module):
+
+            def __init__(self, base_func):
+                super(TupleFunc, self).__init__()
+                self.base_func = base_func
+
+            def forward(self, t, y):
+                return (self.base_func(t, y[0]),)
+
+        class TupleLoss(nn.Module):
+
+            def __init__(self, int_loss):
+                super(TupleLoss, self).__init__()
+                self.int_loss = int_loss
+
+            def forward(self, u, x):
+                return (self.int_loss(u[0], x[0]),)
+
+        tensor_input = True
+        y0 = (y0,)
+        func = TupleFunc(func)
+        if (integer_loss != None):
+            integer_loss = TupleLoss(integer_loss)
+
+    flat_params = _flatten(func.parameters())
+    ys = OdeintAdjointMethod.apply(*y0, func, t, flat_params, rtol, atol, method, options, integer_loss)
+
+    if tensor_input:
+        ys = ys[0]
+    return ys
